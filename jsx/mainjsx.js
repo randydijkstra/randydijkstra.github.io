e2afece15a4bba68ea475a27bbe524c7056a7ed4
Randy Dijkstra <randydijkstra92@gmail.com>
2015-02-17 23:53:13 +0100

    react.js tutorial stuff

---
+++ jsx/mainjsx.js
@@ -0,0 +1,176 @@
+/*
+	- CommentBox
+  		- CommentList
+    		- Comment
+  		- CommentForm
+*/
+
+var CommentBox = React.createClass({
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleCommentSubmit: function(comment) {
+    var comments = this.state.data;
+    var newComments = comments.concat([comment]);
+    this.setState({data: newComments});
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.state.data} />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+      </div>
+    );
+  }
+});
+
+
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+
+var CommentForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.refs.author.getDOMNode().value.trim();
+    var text = this.refs.text.getDOMNode().value.trim();
+    if (!text || !author) {
+      return;
+    }
+    this.props.onCommentSubmit({author: author, text: text});
+    this.refs.author.getDOMNode().value = '';
+    this.refs.text.getDOMNode().value = '';
+  },
+  render: function() {
+    return (
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your name" ref="author" />
+        <input type="text" placeholder="Say something..." ref="text" />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
+
+
+var converter = new Showdown.converter();
+var Comment = React.createClass({
+	render: function() {
+		var rawMarkup = converter.makeHtml(this.props.children.toString());
+		return (
+			<div className="comment">
+			<h3 className="commentAuthor">
+			{this.props.author}
+			</h3>
+			<span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+			</div>
+			);
+	}
+});
+
+
+React.render(
+	<CommentBox url="comments.json" pollInterval={2000}/>,
+	document.getElementById('content')
+	);
+
+//MENU
+var Menu = React.createClass({
+
+	getInitialState: function(){
+		return { focused: 0 };
+	},
+
+	clicked: function(index){
+
+        // The click handler will update the state with
+        // the index of the focused menu entry
+
+        this.setState({focused: index});
+    },
+
+    render: function() {
+
+        // Here we will read the items property, which was passed
+        // as an attribute when the component was created
+
+        var self = this;
+
+        // The map method will loop over the array of menu entries,
+        // and will return a new array with <li> elements.
+
+        return (
+        	<nav>
+        	<ul>{ this.props.items.map(function(m, index){
+
+        		var style = '';
+
+        		if(self.state.focused == index){
+        			style = 'focused';
+        		}
+
+                    // Notice the use of the bind() method. It makes the
+                    // index available to the clicked function:
+
+                    return <li className={style} onClick={self.clicked.bind(self, index)}>{m}</li>;
+
+                }) }
+
+        	</ul>
+
+        	<p>Selected: {this.props.items[this.state.focused]}</p>
+        	</nav>
+        	);
+
+    }
+});
+
+// Render the menu component on the page, and pass an array with menu options
+
+React.render(
+	<Menu items={ ['Home', 'About me', 'Projects', 'Contact me!'] } />,
+	document.getElementById('nav-bar')
+	);
